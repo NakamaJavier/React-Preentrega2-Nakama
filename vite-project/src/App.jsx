ef140e0b996b0ce3a9f5a5445b9629f9e2e0ea1e
JavierNakama <nakamajavier@hotmail.com>
2023-05-16 18:25:22 -0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Header from './components/header/Header'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <Header/>
+      </div>
+  )
+}
+
+export default App
