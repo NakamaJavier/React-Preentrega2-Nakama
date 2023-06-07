b03cac6806b527df61bbceb8a25f72c7e6505636
JavierNakama <nakamajavier@hotmail.com>
2023-06-06 22:36:00 -0300

    Se agregaron componentes, se hizo fetch y se cargo el json de un proyecto anterior. Se agrego un custom hook para usar fetch

--- vite-project/src/components/ItemListContainer/ItemListContainer.jsx
+++ vite-project/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,17 +1,29 @@
+import { useEffect, useState } from "react";
+import ItemCount from "../ItemCount/ItemCount";
 import "./itemlistcontainer.css"
+import Product from "../Product/Product";
+import useFetch from "../../hooks/useFetch";
 
-function ItemListContainer({ greeting }) {
+
+function ItemListContainer() {
+
+    // useEffect(()=>{
+    // fetch("./productos.json")
+    //     .then((response)=>response.json())
+    //     .then((data)=> setProducts(data))
+    // },[])
+    const {data:products,loading}= useFetch ("./productos.json")
+    console.log(products);
     return (
-        <div className="card" style={{ width: "18rem" }}>
-            <img src="/img/bienvenido.webp" className="card-img-top" alt="..." />
-            <div className="card-body">
-            <h5 className="card-title">{ greeting }</h5>
-            <p className="card-text">
-                Sea bienvenido a la página
-            </p>
-            </div>
+        <div className="contenedor d-flex justify-content-start flex-wrap  gap-3">
+        {products &&
+            products.map((product)=>{
+                return(
+                    <Product data={product} key={product.id}/>
+                )
+            })}
         </div>
-    );
+    )
 }
 
 export default ItemListContainer
